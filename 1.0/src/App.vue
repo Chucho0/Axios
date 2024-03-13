7044710d969aaf570833721353fc2c91e216667c
Chucho0 <anonimobuscando666@outlook.com>
2024-03-13 16:33:37 -0500

    En desarrollo

---
+++ 1.0/src/App.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="img">
+        <img :src="img" alt="Pokemon Image" class="card-image">
+      </div>
+      <div class="father">
+        <div class="card-text">
+          <h1>Numero: {{ pokemon.id }}</h1>
+          <h1>Nombre: {{ pokemon.name }}</h1>
+          <h1>Altura: {{ pokemon.height }}</h1>
+          <h1>Peso: {{ pokemon.weight }}</h1>
+        </div>
+        <div class="lineas-text">
+          <p>{{ ataque }}</p>
+          <q-linear-progress reverse :value="ataque2" class="q-mt-md" />
+          <p>{{ defensa }}</p>
+          <q-linear-progress reverse :value="defensa2" class="q-mt-md" />
+          <p>{{ especialataque }}</p>
+          <q-linear-progress reverse :value="especialataque2" class="q-mt-md" />
+          <p>{{ especialdefensa }}</p>
+          <q-linear-progress reverse :value="especialdefensa2" class="q-mt-md" />
+          <p>{{ velocidad }}</p>
+          <q-linear-progress reverse :value="velocidad2" class="q-mt-md" />
+        </div>
+      </div>
+    </div>
+    <button class="card-button" @click="traer()">Traer datos</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+let pokemon = ref({})
+let img = ref('')
+let ataque = ref(0)
+let ataque2 = ref(0)
+let defensa = ref(0)
+let defensa2 = ref(0)
+let especialataque = ref(0)
+let especialataque2 = ref(0)
+let especialdefensa = ref(0)
+let especialdefensa2 = ref(0)
+let velocidad = ref(0)
+let velocidad2 = ref(0)
+async function traer() {
+  try {
+    let random = parseInt(Math.random() * 500 + 1);
+    let r = await axios.get("https://pokeapi.co/api/v2/pokemon/" + random)
+    pokemon.value = r.data
+    console.log(r.data);
+    img.value = r.data.sprites.front_default
+    ataque.value = r.data.stats[1].base_stat
+    ataque2.value = ataque.value / 100
+    defensa.value = r.data.stats[2].base_stat
+    defensa2.value = defensa.value / 100
+    especialataque.value = r.data.stats[3].base_stat
+    especialataque2.value = especialataque.value / 100
+    especialdefensa.value = r.data.stats[4].base_stat
+    especialdefensa2.value = especialdefensa.value / 100
+    velocidad.value = r.data.stats[5].base_stat
+    velocidad2.value = velocidad.value / 100
+  } catch (error) {
+    console.log(error);
+  }
+} 
+</script>
+
+<style scoped>
+.card {
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 60px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 10px 5px 5px black;
+  background: linear-gradient(to bottom,
+      rgb(0, 0, 0),
+      rgb(73, 71, 71));
+}
+
+.card-content {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.img {
+  width: 30%;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.card-image {
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+}
+
+.father {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+}
+
+.card-text {
+  width: 100%;
+}
+
+.card-text h1 {
+  font-size: 27px;
+  text-align: center;
+  height: 50px;
+  color: white;
+}
+
+.lineas-text {
+  width: 100%;
+}
+
+.lineas-text p {
+  margin: 0;
+  font-size: 17px;
+  color: rgb(25, 118, 210);
+}
+
+.card-button {
+  width: 200px;
+  height: 25px;
+  border-radius: 10px;
+  margin-top: auto;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+@media (max-width: 768px) {
+  .img {
+    width: 50%;
+  }
+
+  .father {
+    width: 100%;
+  }
+}
+
+@media (max-width: 480px) {
+  .card-text h1 {
+    font-size: 20px;
+  }
+
+  .lineas-text p {
+    font-size: 15px;
+  }
+
+  .card {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  .card-content {
+    padding: 10px;
+  }
+
+  .father {
+    width: 80%;
+  }
+
+  .card-button {
+    width: 150px;
+  }
+}
+</style>
